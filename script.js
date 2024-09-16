298bffe3c0298c93dbc9aa41a1b8518778af0c20
Ronald-8-ball <ronaldokuk462@gmail.com>
2024-09-16 18:31:18 +0100

    Initialise

---
+++ script.js
@@ -0,0 +1,11 @@
+class student{
+constructor(name,age){
+this.name=name
+this.age=age
+}
+}
+// objects
+// instance of a class
+var student1=new student("Darryl",10)
+document.write(student1.name+"<br>")
+document.write(student1.age)
